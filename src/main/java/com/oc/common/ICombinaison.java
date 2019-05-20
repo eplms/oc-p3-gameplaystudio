3cd28f2eb3bf0e1899c13446011efdd2534c6963
Emmanuel Plumas <eplumas@free.fr>
2019-05-20 08:48:52 +0200

    Implémentation Interface ICombinaison

--- src/main/java/com/oc/common/ICombinaison.java
+++ src/main/java/com/oc/common/ICombinaison.java
@@ -2,4 +2,10 @@ package com.oc.common;
 
 public interface ICombinaison {
 
+	public void genererCombinaisonAleatoire();
+	public void lireCombinaison();
+	public void afficherCombinaison();
+	public void comparerCombinaison();
+	public void genererProchaineCombinaison();
+	
 }
