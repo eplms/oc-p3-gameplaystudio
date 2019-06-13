0ab5a6d6ea29bc4a7aaadee7f43d7d83663dc539
Emmanuel Plumas <eplumas@free.fr>
2019-06-13 18:25:21 +0200

    Ajout de la gestion de saisie de combinaison
    vérification de la saisie utilisateur (nombre de digit et saisie
    d'entier) dans lireCombinaison de la classe Combinaison

--- src/main/java/com/oc/common/Combinaison.java
+++ src/main/java/com/oc/common/Combinaison.java
@@ -1,5 +1,6 @@
 package com.oc.common;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class Combinaison implements ICombinaison{
@@ -48,29 +49,37 @@ public abstract class Combinaison implements ICombinaison{
 	
 	public int[] lireCombinaison (int taille){
 		int combinaison[] = new int[taille];
-		int digit=0;
-			
-		System.out.print("\nVotre Proposition : ");
-		//System.out.println(taille);
-		// saisie direct d'une suite d'un entier à la place d'un String
-		int proposition = saisieCombi.nextInt();
-		
-		// Calcul du nbre de digits de la combinaison lue
-		while ((proposition/(Math.pow(10,digit)))>=1){
-			digit++;
-			}
-		if (digit!=taille) {
-				System.err.println("vous devez entrer une combinaison de "+taille+" chiffres");
-		} else {
+		int digit=0;	
+		int proposition=0;
+		do {
+			System.out.print("\nVotre Proposition : ");
+			// saisie direct d'une suite d'un entier à la place d'un String
 			try {
-				for (int i=0; i<(taille);i++) {
-					combinaison[(taille-i-1)] = (int) (proposition % (Math.pow(10,1)));
-					proposition=proposition/10;
+				proposition = saisieCombi.nextInt();
+				// Calcul du nbre de digits de la combinaison lue
+				digit=0;
+				while ((proposition/(Math.pow(10,digit)))>=1){
+					digit++;
+					}
+				if (digit!=taille) {
+					System.out.println("");
+					System.out.println("Attention !! vous avez saisi une combinaison de "+digit+" chiffres");
+					System.out.println("vous devez entrer une combinaison de "+taille+" chiffres");
+				} else {
+					// Affectation de chaque digit à une case du tableau
+					for (int i=0; i<(taille);i++) {
+						combinaison[(taille-i-1)] = (int) (proposition % (Math.pow(10,1)));
+						proposition=proposition/10;		
+					}
 				}
-			} catch(NumberFormatException ex) {
-				System.err.println("vous devez saisir des chiffres compris entre 0 et 9");
 			}
-		}
+			catch(InputMismatchException e) {
+				// gestion de l'exception en cas de saisie autre que d'entiers
+				System.out.println("Attention ! il ne faut saisir que des chiffres !");
+				//vider le buffer
+				saisieCombi.nextLine();
+			}			
+		}while(digit!=taille);
 		return combinaison;
 	}
 }
